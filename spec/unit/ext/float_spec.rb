b272c75f4de37faeb87520e1475719431b9f6b31
Jamie Monserrate <jamiemonserrate@gmail.com>
2014-09-14 19:35:23 +0530

    jMe - added one more test case

--- spec/unit/ext/float_spec.rb
+++ spec/unit/ext/float_spec.rb
@@ -7,5 +7,11 @@ describe Float do
       expect(1.04.round_to_nearest(0.05)).to eq(1.05)
       expect(1.06.round_to_nearest(0.05)).to eq(1.10)
     end
+
+    it 'should round up to nearest 0.5' do
+      expect(0.1.round_to_nearest(0.5)).to eq(0.5)
+      expect(0.5.round_to_nearest(0.5)).to eq(0.5)
+      expect(0.7.round_to_nearest(0.5)).to eq(1.0)
+    end
   end
 end
